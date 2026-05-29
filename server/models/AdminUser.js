c3fa3283a5141aec389c56497d177055b6ac96ff
PichyyNews <s6702041510113@email.kmutnb.ac.th>
2026-05-29 23:31:16 +0700

    fix: Remove next() from async Mongoose pre-save hook (not supported in newer Mongoose)

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -24,11 +24,10 @@ const adminUserSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-adminUserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+adminUserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 // Compare password method
