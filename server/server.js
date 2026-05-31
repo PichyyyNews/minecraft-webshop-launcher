025a5f963d524190d652d5b4dab440b1bfa0be13
PichyyNews <s6702041510113@email.kmutnb.ac.th>
2026-05-31 22:14:21 +0700

    fix: add x-launcher-version to CORS allowedHeaders

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-launcher-version']
 }));
 
 
